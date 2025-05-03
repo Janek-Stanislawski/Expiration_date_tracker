51eaa6e36749726fe1934659c492d4244bc72055
Janek-Stanislawski <janski9999@gmail.com>
2025-05-03 18:46:25 +0200

    cahnges to the structure

--- frontend.py
+++ frontend.py
@@ -4,6 +4,7 @@ from tkinter import ttk
 
 background = "#1f9aa1"
 
+
 def test():
     style = ttk.Style()
     style.configure(
@@ -12,6 +13,7 @@ def test():
         background=background,
         font=("TkDefaultFont", 40)
         )
+
 
 class Window:
 
@@ -23,34 +25,44 @@ class Window:
         self.window["background"] = background
 
         self.button = tk.Button(text="Add Item")
-        self.button.place(x=270, y=80)
-        self.button.bind("<Button-1>", self.handle_button_press)
         self.button_description = tk.Label(self.window, text="Click this button to add item to tracker",
                                            bg=background)
+
+        self.name_entry = Entry(self.window, width=15)
+        self.day_entry = Entry(self.window, width=2)
+        self.month_entry = Entry(self.window, width=2)
+        self.year_entry = Entry(self.window, width=4)
+
+        self.date_entry_label_sep_dot1 = tk.Label(self.window, text=".", bg=background, font=("Helvetica", 15))
+        self.date_entry_label_sep_dot2 = tk.Label(self.window, text=".", bg=background, font=("Helvetica", 15))
+
+    def window1(self):
+
+        self.button.place(x=270, y=80)
+        self.button.bind("<Button-1>", self.handle_button_press)
+
         self.button_description.place(x=30, y=80)
         self.button.bind()
 
-        self.name_entry = Entry(self.window, width=15)
         self.name_entry.place(x=30, y=40)
-        self.day_entry = Entry(self.window, width=2)
+
         self.day_entry.place(x=150, y=40)
-        self.month_entry = Entry(self.window, width=2)
+
         self.month_entry.place(x=175, y=40)
-        self.year_entry = Entry(self.window, width=4)
+
         self.year_entry.place(x=200, y=40)
-        self.date_entry_label1 = tk.Label(self.window, text=".", bg=background, font=("Helvetica", 15))
-        self.date_entry_label1.place(x=165, y=35)
-        self.date_entry_label1.lower()
-        self.date_entry_label2 = tk.Label(self.window, text=".", bg=background, font=("Helvetica", 15))
-        self.date_entry_label2.place(x=190, y=35)
-        self.date_entry_label2.lower()
+
+        self.date_entry_label_sep_dot1.place(x=165, y=35)
+        self.date_entry_label_sep_dot1.lower()
+
+        self.date_entry_label_sep_dot2.place(x=190, y=35)
+        self.date_entry_label_sep_dot2.lower()
 
     def handle_button_press(self, event):
         return
 
-    def main(self):
-        return
-
 
 if __name__ == "__main__":
-    Window().window.mainloop()
+    app = Window()
+    app.window1()
+    app.window.mainloop()
